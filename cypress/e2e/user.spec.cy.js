c1c48e335f8e85cc37aef767fb39b6bc47046391
QA-Henrsb <henrsb@gmail.com>
2025-10-07 18:46:13 -0300

    Created/separated login.spec.cy and user.spec.cy.js cenarios to better organization

---
+++ cypress/e2e/user.spec.cy.js
@@ -0,0 +1,32 @@
+import UserData from '../fixtures/users/user-data.json'
+import LoginPage from '../pages/loginPage.js'
+import DashboardPage from '../pages/dashboardPage.js'
+import MyInfoPage from '../pages/myInfoPage.js'
+import MenuPage from '../pages/menuPage.js'
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage()
+const myInfoPage = new MyInfoPage()
+const menuPage = new MenuPage()
+describe('Orange HRM  Test', () => {
+
+  
+  
+
+  it(' User Info Update - Sucess', () => {
+
+    loginPage.acessLoginPage()
+    loginPage.loginWithUser(UserData.userSucess.username, UserData.userSucess.password)
+    dashboardPage.verifyDashboardPage()
+    menuPage.clickMyInfoButton()
+    myInfoPage.updateMyPersonalDetails('Henrique', 'Biver')
+    myInfoPage.updateMyEmployeeDetails('333','666','999','2025-06-10','1994-20-06')
+    myInfoPage.updateCountry()
+    myInfoPage.updateMarital()
+    myInfoPage.submitButton()
+    
+    
+  })
+
+  
+})
+
