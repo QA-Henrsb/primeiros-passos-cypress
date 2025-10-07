19c5f398d69de2b9473a72f5ed93ed37538785a6
QA-Henrsb <henrsb@gmail.com>
2025-10-06 22:58:58 -0300

    Added to #1 cenario: dropdown selection automation to country/marital status

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -11,8 +11,10 @@ describe('Orange HRM  Test', () => {
     firstNameField: "[name='firstName']",
     lastNameField: "[name='lastName']",
     genericField : ".oxd-input--active",
-    submitButton : "[type='submit']"
-
+    submitButton : "[type='submit']",
+    genericArrow : ".oxd-select-text",
+    countryBox : ".oxd-select-dropdown > :nth-child(27)",
+    maritalBox : ".oxd-select-dropdown > :nth-child(3)"
   }
 
   
@@ -30,9 +32,15 @@ describe('Orange HRM  Test', () => {
     cy.get(selectorsList.genericField).eq(3).clear().type('333')
     cy.get(selectorsList.genericField).eq(4).clear().type('666')
     cy.get(selectorsList.genericField).eq(5).clear().type('999')
-    cy.get(selectorsList.genericField).eq(6).clear().click().type('10-2025-06')
-    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get(selectorsList.genericField).eq(6).clear().click().type('2025-06-10')
+    cy.get(selectorsList.genericField).eq(7).clear().click().type('1994-20-06')
+    cy.get(selectorsList.genericArrow).eq(0).click({force: true})
+    cy.get(selectorsList.countryBox).click()
+    cy.get(selectorsList.genericArrow).eq(1).click({force: true})
+    cy.get(selectorsList.maritalBox).click()
+    cy.get(selectorsList.submitButton).eq(0).click({force: true})
     cy.get('.oxd-toast-close')
+    
   })
 
   it('Login - Failure', () => {
